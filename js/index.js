4414bd5b1f7fda7956a0a6a287a5c37f7b09f625
Glenn Searby <glenn.searby@gmail.com>
2015-06-01 10:36:52 +0100

    Made this a little more robust in the case that the iframe get deleted.

--- js/index.js
+++ js/index.js
@@ -2,25 +2,40 @@
 
 /*global module, require*/
 
-var d3 = require("d3");
+var d3 = require("d3"),
+    pointerEvents = "pointer-events";
 
 module.exports = function() {
     var disable = 0;
     
-    if (window.frameElement) {
+	if (window.frameElement) {
+	    var iframe = window.frameElement;
+	    
 	window.parent.addEventListener("scroll", function() {
-	    if (!disable) {
-		window.frameElement.style.pointerEvents = "none";
+	    if (!iframe) {
+		return;
+	    }
+
+	    if (disable === 0) {
+		d3.select(iframe)
+		    .style(pointerEvents, "none");
 	    }
 	    
 	    disable++;
 	    
 	    window.setTimeout(
 		function() {
-		    disable--;
+		    if (disable > 0) {
+			disable--;
+		    }
 
-		    if (!disable) {
-			window.frameElement.style.pointerEvents = "auto";
+		    if (!iframe) {
+			return;
+		    }
+
+		    if (disable === 0) {
+			d3.select(iframe)
+			    .style(pointerEvents, "auto");
 		    }
 		},
 		500
